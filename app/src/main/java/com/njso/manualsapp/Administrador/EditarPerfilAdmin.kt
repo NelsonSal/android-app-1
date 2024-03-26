f17a951b877b56ce7934d42f9920a142af31b32d
NelsonJ <njsocode@gmail.com>
2024-03-26 15:30:22 -0500

    inicio 46

--- app/src/main/java/com/njso/manualsapp/Administrador/EditarPerfilAdmin.kt
+++ app/src/main/java/com/njso/manualsapp/Administrador/EditarPerfilAdmin.kt
@@ -1,15 +1,25 @@
 package com.njso.manualsapp.Administrador
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 import com.njso.manualsapp.R
 import com.njso.manualsapp.databinding.ActivityEditarPerfilAdminBinding
 
@@ -20,6 +30,8 @@ class EditarPerfilAdmin : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
 
     private lateinit var progressDialog : ProgressDialog
+
+    private var imagenUri : Uri?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +46,55 @@ class EditarPerfilAdmin : AppCompatActivity() {
         progressDialog.setTitle("Espere por favor")
         progressDialog.setCanceledOnTouchOutside(false)
 
+        binding.FbCambiarImagen.setOnClickListener {
+            mostrarOpciones()
+        }
+
         binding.BtnActualizarInfo.setOnClickListener {
             validarinformacion()
         }
     }
+
+    private fun mostrarOpciones() {
+        val popupMenu = PopupMenu(this, binding.imgPerfilAdmin)
+        popupMenu.menu.add(Menu.NONE,0,0,"Galería")
+        popupMenu.menu.add(Menu.NONE,0,0,"Cámara")
+        popupMenu.show()
+
+        popupMenu.setOnMenuItemClickListener { item->
+            val id = item.itemId
+            if (id==0){
+                //Elegir una imagen de la galería
+                seleccionarImgGaleria()
+
+            }else if (id==1){
+                //Tomar fotografía
+
+            }
+            true
+        }
+    }
+
+    private fun seleccionarImgGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type="image/*"
+        ARLGAleria.launch(intent)
+
+    }
+    private val ARLGAleria=registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult>{resultado->
+            if(resultado.resultCode== Activity.RESULT_OK){
+                val data = resultado.data
+                imagenUri=data!!.data
+
+                binding.imgPerfilAdmin.setImageURI(imagenUri)
+            }else {
+                Toast.makeText(applicationContext,"Cancelado", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    )
 
     private var nombres=""
     private fun validarinformacion() {
@@ -45,10 +102,66 @@ class EditarPerfilAdmin : AppCompatActivity() {
         if (nombres.isEmpty()){
             Toast.makeText(applicationContext, "Ingrese un nuevo nombre",Toast.LENGTH_SHORT).show()
         }else {
-            actualizarinformacion()
+            if (imagenUri==null){
+                actualizarinformacion()
+            }else{
+                subirImagenStorage()
+            }
+
+
+
         }
 
     }
+
+    private fun subirImagenStorage() {
+        progressDialog.setMessage("Subiendo Imagen a Storage")
+        progressDialog.show()
+
+        val rutaImagen = "ImagenesPerfilAdministradores/"+firebaseAuth.uid
+        val ref = FirebaseStorage.getInstance().getReference(rutaImagen)
+        ref.putFile(imagenUri!!)
+            .addOnSuccessListener {taskSnapshot->
+                val uriTask : Task<Uri> = taskSnapshot.storage.downloadUrl
+                while (!uriTask.isSuccessful);
+                val urlImagen = "${uriTask.result}"
+                subirImagenDatabase(urlImagen)
+
+
+
+            }
+            .addOnFailureListener {e->
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext,"error producido  ${e.message}",Toast.LENGTH_SHORT).show()
+
+            }
+    }
+
+    private fun subirImagenDatabase(urlImagen : String) {
+        progressDialog.setMessage("Actualizando Imagen")
+        val hashMap : HashMap<String,Any> = HashMap()
+        if (imagenUri != null){
+            hashMap["imagen"] = urlImagen
+
+        }
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(firebaseAuth.uid!!)
+            .updateChildren(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext,"Su imagen se ha actualizado",Toast.LENGTH_SHORT).show()
+
+            }
+            .addOnFailureListener {e->
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext,"error producido:  ${e.message}",Toast.LENGTH_SHORT).show()
+
+
+            }
+
+
+    }
+
 
     private fun actualizarinformacion() {
         progressDialog.setMessage("Actualizando información")
